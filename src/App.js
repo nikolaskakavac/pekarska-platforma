24f627d1a4b7f3c2af71e21f18325e229ad67ad7
Nikolask Kakavac <nikolaskakavac@gmail.com>
2026-02-03 01:38:17 +0100

    Use HashRouter to fix GitHub Pages refresh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, Navigate, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, Navigate, useNavigate } from 'react-router-dom';
 import './App.css';
 import CourseHeader from './components/CourseHeader';
 import HomePage from './pages/HomePage';
